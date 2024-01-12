b6be151c5cf96955bcecb24fb6b7376249de85fc
ktj1997 <rlaxowns7916@gmail.com>
2024-01-12 16:45:58 +0900

    feat: Swagger

--- core-api/src/main/java/com/imlinker/coreapi/core/system/SystemController.java
+++ core-api/src/main/java/com/imlinker/coreapi/core/system/SystemController.java
@@ -5,18 +5,25 @@ import com.imlinker.error.ApplicationException;
 import com.imlinker.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class HealthController {
+public class SystemController {
 
     @GetMapping("/ping")
     public ApiResponse<String> ping() {
         log.info("[HealthController][Ping]");
         return ApiResponse.success("pong");
+    }
+
+    @GetMapping("favicon.ico")
+    public HttpEntity<?> favicon() {
+        return ResponseEntity.EMPTY;
     }
 
     @GetMapping("/unhandled-exception")
