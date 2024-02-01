02f729e21d8cf5f8127a9ed948e90121b5695c43
ktj1997 <rlaxowns7916@gmail.com>
2024-02-02 03:02:28 +0900

    [LINKER-X] fix: AccessToken Expire여부 catch

--- core-api/src/main/java/com/imlinker/coreapi/core/auth/security/jwt/JwtTokenRefresher.java
+++ core-api/src/main/java/com/imlinker/coreapi/core/auth/security/jwt/JwtTokenRefresher.java
@@ -4,6 +4,7 @@ import com.imlinker.domain.auth.AuthService;
 import com.imlinker.domain.user.model.User;
 import com.imlinker.error.ApplicationException;
 import com.imlinker.error.ErrorType;
+import io.jsonwebtoken.ExpiredJwtException;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +19,21 @@ public class JwtTokenRefresher {
     private final AuthService authService;
 
     public TokenResponse refresh(String accessToken, String refreshToken) {
-        Long userId =
-                Long.parseLong(
-                        provider.parseClaims(accessToken, TokenType.ACCESS_TOKEN).get("id").toString());
-        User user = authService.findByUserId(userId);
+        try {
+            Long userId =
+                    Long.parseLong(
+                            provider.parseClaims(accessToken, TokenType.ACCESS_TOKEN).get("id").toString());
+            log.info("[토큰][재발급][시작] (expired=false, userId={})", userId);
+            return refresh(userId, refreshToken);
+        } catch (ExpiredJwtException e) {
+            Long userId = Long.parseLong(e.getClaims().get("id").toString());
+            log.info("[토큰][재발급][시작] (expired=true, userId={})", userId);
+            return refresh(userId, refreshToken);
+        }
+    }
 
+    private TokenResponse refresh(Long userId, String refreshToken) {
+        User user = authService.findByUserId(userId);
         if (user.getRefreshToken() == null || !user.getRefreshToken().equals(refreshToken)) {
             log.info("[AccessToken][재발급][실패] refreshToken이 존재하지 않거나, 일치하지 않음 (userId={})", userId);
             throw new ApplicationException(ErrorType.UNAUTHORIZED);
