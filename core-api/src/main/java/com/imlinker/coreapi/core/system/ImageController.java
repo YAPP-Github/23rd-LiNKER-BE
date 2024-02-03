ddbf69e057a9ad361140fb309e6782d739f18d03
ktj1997 <rlaxowns7916@gmail.com>
2024-02-03 18:17:45 +0900

    [LINKER-X] feat: 이미지 삭제하기 MockApi추가

--- core-api/src/main/java/com/imlinker/coreapi/core/system/ImageController.java
+++ core-api/src/main/java/com/imlinker/coreapi/core/system/ImageController.java
@@ -6,15 +6,14 @@ import com.imlinker.coreapi.core.system.response.UploadImageResponse;
 import com.imlinker.coreapi.support.response.ApiResponse;
 import com.imlinker.domain.common.file.FileService;
 import com.imlinker.domain.common.model.URL;
+import com.imlinker.enums.OperationResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -35,5 +34,14 @@ public class ImageController {
             @AuthenticatedUserContext AuthenticatedUserContextHolder authenticatedUserContextHolder) {
         URL uploadedImageUrl = service.uploadImage(authenticatedUserContextHolder.getId(), file);
         return ApiResponse.success(new UploadImageResponse(uploadedImageUrl.getValue()));
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "이미지 삭제하기 (mock)")
+    public ApiResponse<OperationResult> deleteImages(
+            @RequestParam List<String> imageUrls,
+            @AuthenticatedUserContext AuthenticatedUserContextHolder authenticatedUserContextHolder) {
+
+        return ApiResponse.success(OperationResult.SUCCESS);
     }
 }
