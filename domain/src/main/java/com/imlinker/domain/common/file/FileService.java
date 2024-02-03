3baa48bc018ed67165188ccf22b2da55d3447b36
ktj1997 <rlaxowns7916@gmail.com>
2024-02-03 17:50:43 +0900

    [LINKER-X] FileNaming 변경

--- domain/src/main/java/com/imlinker/domain/common/file/FileService.java
+++ domain/src/main/java/com/imlinker/domain/common/file/FileService.java
@@ -2,6 +2,7 @@ package com.imlinker.domain.common.file;
 
 import com.imlinker.domain.common.model.URL;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +14,8 @@ public class FileService {
     private final FileUploader fileUploader;
 
     public URL uploadImage(Long userId, MultipartFile file) {
-        String fileName = String.format("user:%s-profile-%s", userId, LocalDateTime.now());
+        String fileName =
+                String.format("user:%s:%s:%s", userId, LocalDateTime.now(), UUID.randomUUID());
         UploadFile uploadFile = new UploadFile(fileName, FileType.IMAGE, file);
         return fileUploader.uploadImage(uploadFile);
     }
